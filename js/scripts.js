6e485ce983586f972f408adf0064acdf44271a39
Guest User <Guest@epicodus-35.local>
2016-11-09 18:26:34 -0800

    create a webapp that allows the user to choose a pizza size, and toppings using a pizza object. Also uses a prototype method to determine the price.

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+// Back-end logic
+var toppingsArray = [];
+var toppingsCost;
+var toppingsAsString;
+
+function pizza (size, toppings, cost) {
+  this.size = size;
+  this.toppings = toppings;
+  this.cost = cost;
+};
+
+var newPizza = new pizza("size", [], 0)
+
+pizza.prototype.pizzaCost = function() {
+  if (newPizza.size === "personal") {
+    newPizza.cost += 10;
+  } else if (newPizza.size === "medium") {
+    newPizza.cost += 14;
+  } else if (newPizza.size === "large") {
+    newPizza.cost += 18;
+  } else if (newPizza.size === "family") {
+    newPizza.cost += 22
+  }
+  toppingsCost = toppingsArray.length;
+  newPizza.cost += toppingsCost;
+}
+
+
+// Front-end logic
+$(document).ready(function() {
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    toppingsArray = [];
+
+    $("input:checkbox[name=topping]:checked").each(function() {
+      var allToppings = $(this).val();
+      toppingsArray.push(allToppings);
+    });
+
+    toppingsAsString = toppingsArray.join(", ");
+
+    newPizza = new pizza (
+    $("#size option:selected").val(),
+    toppingsArray,
+    0);
+
+    newPizza.pizzaCost();
+
+    $("div#output").text("You ordered a " + newPizza.size + " " + toppingsAsString + " " + "pizza and your total is $" + newPizza.cost);
+    console.log(toppingsArray);
+  });
+});
